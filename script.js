6517e97384e3b64bc583fa24175ba105743d4cc4
rib9985 <rib9985@gmail.com>
2022-09-09 23:15:28 -0300

    Add functions

---
+++ script.js
@@ -0,0 +1,34 @@
+const add = function(a,b) {
+    return a + b
+  };
+  
+  const subtract = function(a,b) {
+  return a - b
+      
+  };
+  
+  const sum = function(array) {
+  return array.reduce((previousValue, currentValue) => previousValue + currentValue, 0)
+  
+  };
+  
+  const multiply = function(array) {
+    
+  return array.reduce((accumulate,nextItem) => 
+  accumulate * nextItem)
+  
+  };
+  
+  const power = function(a,b) {
+   return a ** b
+      
+  };
+  
+  const factorial = function(a) {
+      const store = a
+    total = 1
+  for (let i = 1; i<=store; i++){
+    total*=i
+  }
+  return total
+  }
