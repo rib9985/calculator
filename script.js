e782ec685772a8eb5b24eb007ae525edb9218a1e
rib9985 <rib9985@gmail.com>
2023-04-05 13:02:39 -0300

    Add eslint package to project

--- script.js
+++ script.js
@@ -1,60 +1,54 @@
-//Initial Variables for the events
-let operator = "";
-let firstOperand = "";
-let secondOperand = "";
+// Initial Variables for the events
+let operator = '';
+let firstOperand = '';
+let secondOperand = '';
 let currentOperation = null;
 let mustResetScreen = false;
 
-//Grab elements from the DOM, pass them to constant variables
-const numberButtons = document.querySelectorAll(".btn");
-const operatorButtons = document.querySelectorAll(".operator");
-const clearButton = document.getElementById("clear");
-const evaluateButton = document.getElementById("evaluate");
-const lastOperationScreen = document.getElementById("previousScreen");
-const currentOperationScreen = document.getElementById("mainScreen");
-const operatorScreen = document.getElementById("operatorScreen");
-const deleteButton = document.getElementById("delete");
-const decimalButton = document.getElementById("decimal");
+// Grab elements from the DOM, pass them to constant variables
+const numberButtons = document.querySelectorAll('.btn');
+const operatorButtons = document.querySelectorAll('.operator');
+const clearButton = document.getElementById('clear');
+const evaluateButton = document.getElementById('evaluate');
+const lastOperationScreen = document.getElementById('previousScreen');
+const currentOperationScreen = document.getElementById('mainScreen');
+const operatorScreen = document.getElementById('operatorScreen');
+const deleteButton = document.getElementById('delete');
+const decimalButton = document.getElementById('decimal');
 
-//Events that happen on each button click
-numberButtons.forEach((button) =>
-  button.addEventListener("click", () => addNumberToScreen(button.textContent))
-);
-operatorButtons.forEach((button) =>
-  button.addEventListener("click", () => setOperator(button.textContent))
-);
-evaluateButton.addEventListener("click", () =>
-  determineOperation(operator, true)
-);
-clearButton.addEventListener("click", () => resetAllParameters());
-deleteButton.addEventListener("click", () => removeNumberFromScreen());
-decimalButton.addEventListener("click", () => checkDecimal());
+// Events that happen on each button click
+numberButtons.forEach((button) => button.addEventListener('click', () => addNumberToScreen(button.textContent)));
+operatorButtons.forEach((button) => button.addEventListener('click', () => setOperator(button.textContent)));
+evaluateButton.addEventListener('click', () => determineOperation(operator, true));
+clearButton.addEventListener('click', () => resetAllParameters());
+deleteButton.addEventListener('click', () => removeNumberFromScreen());
+decimalButton.addEventListener('click', () => checkDecimal());
 
-//Resets to initial state, with no variable values and empty strings.
+// Resets to initial state, with no variable values and empty strings.
 function resetAllParameters() {
   currentOperation = null;
-  operator = "";
-  mainScreen.textContent = "";
-  previousScreen.textContent = "";
-  operatorScreen.textContent = "";
+  operator = '';
+  mainScreen.textContent = '';
+  previousScreen.textContent = '';
+  operatorScreen.textContent = '';
 }
 
-//Removes the last number from the screen
+// Removes the last number from the screen
 function removeNumberFromScreen() {
-  let length = currentOperationScreen.textContent.length;
-  let newString = currentOperationScreen.textContent.slice(0, length - 1);
+  const { length } = currentOperationScreen.textContent;
+  const newString = currentOperationScreen.textContent.slice(0, length - 1);
   return (currentOperationScreen.textContent = newString);
 }
 
-//Adds/Appends the number chosen to the screen.
+// Adds/Appends the number chosen to the screen.
 function addNumberToScreen(number) {
-  if (currentOperationScreen.textContent === "ERR") {
-    previousScreen.textContent = "";
+  if (currentOperationScreen.textContent === 'ERR') {
+    previousScreen.textContent = '';
     currentOperation = null;
-    operator = "";
+    operator = '';
   }
 
-  if (currentOperationScreen.textContent === "0" || mustResetScreen) {
+  if (currentOperationScreen.textContent === '0' || mustResetScreen) {
     resetScreen();
   }
 
@@ -63,28 +57,28 @@ function addNumberToScreen(number) {
 
 function checkDecimal() {
   const checkerScreen = currentOperationScreen.textContent;
-  for (let c of checkerScreen) {
-    if (c === ".") {
+  for (const c of checkerScreen) {
+    if (c === '.') {
       return;
     }
   }
-  addNumberToScreen(".");
+  addNumberToScreen('.');
 }
 
-//Sets the Operator value globally
+// Sets the Operator value globally
 function setOperator(op) {
   return (operator = op) && determineOperation(operator, false);
 }
 
-//Determines how to handle the operation, and passes on the equivalent values.
-//Also stores the currentOperation
+// Determines how to handle the operation, and passes on the equivalent values.
+// Also stores the currentOperation
 function determineOperation(op, equal) {
-  if (currentOperation !== ("" || null)) {
+  if (currentOperation !== ('' || null)) {
     evaluate(op);
   }
 
   if (equal === true) {
-    updateLastOperationScreen("=");
+    updateLastOperationScreen('=');
   } else {
     updateLastOperationScreen(op);
   }
@@ -92,14 +86,14 @@ function determineOperation(op, equal) {
   currentOperation = `${firstOperand}`;
 }
 
-//Stores the 1st Operand
+// Stores the 1st Operand
 function storeOperand() {
   firstOperand = currentOperationScreen.textContent;
 }
 
-//Updates the Operator screen and the Previous Operation Screen
+// Updates the Operator screen and the Previous Operation Screen
 function updateLastOperationScreen(op) {
-  if (op == "=") {
+  if (op == '=') {
     lastOperationScreen.textContent = `${firstOperand} ${operator} ${secondOperand}`;
     operatorScreen.textContent = op;
   } else {
@@ -109,14 +103,14 @@ function updateLastOperationScreen(op) {
   mustResetScreen = true;
 }
 
-//Resets the mainScreen content
+// Resets the mainScreen content
 function resetScreen() {
-  mainScreen.textContent = "";
+  mainScreen.textContent = '';
   mustResetScreen = false;
 }
 
-//Evaluater function
-//Evaluates what to do once an operator is chosen.
+// Evaluater function
+// Evaluates what to do once an operator is chosen.
 function evaluate(operator) {
   if (currentOperation === null || mustResetScreen) {
     return;
@@ -126,29 +120,29 @@ function evaluate(operator) {
   currentOperationScreen.textContent = `${operate(
     firstOperand,
     operator,
-    secondOperand
+    secondOperand,
   )}`;
   previousScreen.textContent = currentOperationScreen.textContent;
 }
 
-//Main operation function.
+// Main operation function.
 // Receives the full parameters and return the value depending on the operation.
 function operate(firstOperand, operator, secondOperand) {
   a = Number(firstOperand);
   b = Number(secondOperand);
-  if (operator === "+") {
+  if (operator === '+') {
     return add(a, b);
-  } else if (operator === "-") {
+  } if (operator === '-') {
     return subtract(a, b);
-  } else if (operator === "x") {
+  } if (operator === 'x') {
     return multiply(a, b);
-  } else if (operator === "/") {
-    if (b === 0 || null || undefined) return "ERR";
-    else return divide(a, b).toFixed(5);
-  } else return "ERR";
+  } if (operator === '/') {
+    if (b === 0 || null || undefined) return 'ERR';
+    return divide(a, b).toFixed(5);
+  } return 'ERR';
 }
 
-//Smaller Operations
+// Smaller Operations
 const add = function (a, b) {
   return a + b;
 };
@@ -162,10 +156,10 @@ const divide = function (a, b) {
   return a / b;
 };
 
-//Keystroke Functions
-//Registers keystrokes accordingly to each button
+// Keystroke Functions
+// Registers keystrokes accordingly to each button
 
-document.addEventListener("keydown", (e) => {
+document.addEventListener('keydown', (e) => {
   numberButtons.forEach((button) => {
     if (e.key === button.textContent) {
       addNumberToScreen(button.textContent);
@@ -173,7 +167,7 @@ document.addEventListener("keydown", (e) => {
   });
 });
 
-document.addEventListener("keydown", (e) => {
+document.addEventListener('keydown', (e) => {
   operatorButtons.forEach((button) => {
     if (e.key === button.textContent) {
       setOperator(button.textContent);
@@ -181,32 +175,32 @@ document.addEventListener("keydown", (e) => {
   });
 });
 
-document.addEventListener("keydown", (e) => {
-  if (e.key === "*") {
-    setOperator("x");
+document.addEventListener('keydown', (e) => {
+  if (e.key === '*') {
+    setOperator('x');
   }
 });
 
-document.addEventListener("keydown", (e) => {
-  if (e.key === "Escape") {
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
     resetAllParameters();
   }
 });
 
-document.addEventListener("keydown", (e) => {
-  if (e.key === "Backspace") {
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Backspace') {
     removeNumberFromScreen();
   }
 });
 
-document.addEventListener("keydown", (e) => {
-  if (e.key === ".") {
+document.addEventListener('keydown', (e) => {
+  if (e.key === '.') {
     checkDecimal();
   }
 });
 
-document.addEventListener("keydown", (e) => {
-  if (e.key === "=" || e.key === "Enter") {
+document.addEventListener('keydown', (e) => {
+  if (e.key === '=' || e.key === 'Enter') {
     determineOperation(operator, true);
   }
 });
